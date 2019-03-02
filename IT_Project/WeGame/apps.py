f1fac5091c58bf053788e9e918e1ba69a72bb25f
Ellison Zhang <ellison@glaroam2-189-143.wireless.gla.ac.uk>
2019-03-02 15:30:56 +0000

    url

---
+++ IT_Project/WeGame/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WegameConfig(AppConfig):
+    name = 'WeGame'
---
+++ IT_Project/WeGame/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from WeGame import views
+
+# app_name = 'WeGame'
+
+urlpatterns = [
+    
+
+]
