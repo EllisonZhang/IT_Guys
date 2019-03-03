572a11041895329c07975b4cda6934361458f63f
Ellison Zhang <ellison@Ellisons-MacBook-Pro-2.local>
2019-03-03 14:39:13 +0000

    RUA

--- IT_Project/IT_Project/settings.py
+++ IT_Project/IT_Project/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'WeGame',
+    'registration' # add in the registration package
 ]
 
 MIDDLEWARE = [
--- IT_Project/WeGame/urls.py
+++ IT_Project/WeGame/urls.py
@@ -4,6 +4,9 @@ from WeGame import views
 # app_name = 'WeGame'
 
 urlpatterns = [
-    
-
+    url(r'^$', views.index, name = 'index'),
+    url(r'^register/$', views.register, name = 'register'),
+    url(r'^login/$', views.user_login, name='login'),
+    # url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^logout/$', views.user_logout, name='logout'),
 ]
