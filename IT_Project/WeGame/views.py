673f22895ba885d802314e71c9173becb361c882
Kevin <kevin.kengne1@gmail.com>
2019-03-03 15:05:15 +0000

    Views added

--- IT_Project/WeGame/views.py
+++ IT_Project/WeGame/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    response = render(request, 'wegame/index.html')
+    return response
+
+def about(request):
+    response = render(request, 'wegame/about.html')
+    return response
+
+def login(request):
+    response = render(request, 'wegame/login.html')
+    return response
+
+def logout(request):
+    response = render(request, 'wegame/logout.html')
+    return response
+
+def register(request):
+    response = render(request, 'wegame/register.html')
+    return response
+
