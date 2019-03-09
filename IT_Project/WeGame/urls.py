7acda862ebce2960e18c85d08efb78088ce73005
goldedda <edmund@operamail.com>
2019-03-09 12:58:09 +0000

    about page

--- IT_Project/WeGame/urls.py
+++ IT_Project/WeGame/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     # url(r'^restricted/', views.restricted, name='restricted'),
     url(r'^logout/$', views.logout, name='logout'),
     url(r'^games/$', views.games, name='games'),
+    url(r'^games/$', views.about, name='about'),
 ]
