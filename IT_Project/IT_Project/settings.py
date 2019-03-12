e49f59d8850623242e21dbb7cfbfd2f3ad885e7e
Kevin <kevin.kengne1@gmail.com>
2019-03-12 18:31:36 +0000

    Photo registration

--- IT_Project/IT_Project/settings.py
+++ IT_Project/IT_Project/settings.py
@@ -5,6 +5,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -132,6 +133,7 @@ USE_TZ = True
 STATICFILES_DIRS = [STATIC_DIR,]
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 
