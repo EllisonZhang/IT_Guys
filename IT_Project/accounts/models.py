450a254107e06e74684721cd322d6d2f5b1d08f6
Kevin <kevin.kengne1@gmail.com>
2019-03-20 20:08:08 +0000

    Populated advanced

--- IT_Project/accounts/models.py
+++ IT_Project/accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(default=0)
     photo = models.ImageField(upload_to='profilePictures', null=True)
+
--- IT_Project/populate_wegame.py
+++ IT_Project/populate_wegame.py
@@ -4,6 +4,8 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 import django
 django.setup()
 from WeGame.models import Publisher,Game,Picture,Review,Video
+from accounts.models import CustomUser
+from PIL import Image
 
 def populate():
 # First, we will create lists of dictionaries containing the pages
@@ -119,27 +121,32 @@ def populate():
     ]
 
     reviews = [
-        {"game_name":"ARK:Survival Invovled","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"The Binding of Isaac: Rebirth","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"Don't Starve Together","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"Euro Truck Simulator 2","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"The Forest","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"Hearthstone","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"OVERWATCH","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"Stardew Valley","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
-        {"game_name":"WORLD OF WARCRFT","user_name":"czx","number_likes":"2","number_dislikes":"3","text":"good"},
+        {"game_name":"ARK:Survival Invovled","username":"EdmundKuras","number_likes":"2","number_dislikes":"3","text":"Ark’s lofty ambitions and 3D graphics make for a demanding game that can make your battery beg for mercy. Assuming that your device isn’t too prehistoric (the last couple of generations only need apply) then you can download and begin playing for free. "},
+        {"game_name":"The Binding of Isaac: Rebirth","username":"Nelson","number_likes":"2","number_dislikes":"3","text":"The Binding of Isaac is the reference for the rogue-like genre - even if you already know the old Isaac inside out."},
+        {"game_name":"Don't Starve Together","username":"EllisonZhang","number_likes":"2","number_dislikes":"3","text":"Don’t Starve Together is a multiplayer strategy sandbox game where every action a player makes can affect the player’s future. "},
+        {"game_name":"Euro Truck Simulator 2","username":"GerardoAragonCamarasa","number_likes":"2","number_dislikes":"3","text":"A surprising truly well-done simulation that puts you behind a truck letting you enjoy discovering and exploring Europe."},
+        {"game_name":"The Forest","username":"GerardoAragonCamarasa","number_likes":"2","number_dislikes":"3","text":"The Forest frequently flirts with perfection. A fantastic and engrossing survival-horror experience from start to finish."},
+        {"game_name":"Hearthstone","username":"GerardoAragonCamarasa","number_likes":"2","number_dislikes":"3","text":"Hearthstone, the newest card game from Blizzard, will keep you hooked for hours thanks to the amazing presentation and the tactical, deep gameplay. This is free to play done right."},
+        {"game_name":"OVERWATCH","username":"KevinKengne","number_likes":"2","number_dislikes":"3","text":"Easily one of the best multiplayer first person shooters. Vastly better than Team Fortress 2, which i used to play in the past."},
+        {"game_name":"Stardew Valley","username":"KevinKengne","number_likes":"2","number_dislikes":"3","text":"The charming RPG Stardew Valley is so much more than just another farming simulator and feels right at home on the Switch."},
+        {"game_name":"WORLD OF WARCRFT","username":"KevinKengne","number_likes":"2","number_dislikes":"3","text":"Battle of Azeroth isn’t as good as Legion was. But the Horde vs Alliance conflict is stronger than ever, and being part of this big conflict is pure fun. Veterans will enjoy HL content and how the lore is constantly used."},
+        {"game_name":"ARK:Survival Invovled","username":"EdmundKuras","number_likes":"2","number_dislikes":"3","text":"The developers state that it is possible to obtain every available structure, item and enhancement without having to spend any money. Free gifts pop up every couple of hours that can be redeemed by watching ads."},
+        {"game_name":"The Binding of Isaac: Rebirth","username":"EdmundKuras","number_likes":"2","number_dislikes":"3","text":"Crazier, funnier and bigger. Rebirth is a proper rebuilt of one of the most brilliant games we've enjoyed lately. There are a handful of new features and probably even more content than we could've asked for, even though die-hard fans might consider rebuying a game that's very much the same, but widely expanded."},
+        {"game_name":"Don't Starve Together","username":"EdmundKuras","number_likes":"2","number_dislikes":"3","text":"Don't Starve Together does little to fix the original's glaring problems, but still, conquering the wilderness together with friends is a lot of fun."},
+        {"game_name":"Euro Truck Simulator 2","username":"Nelson","number_likes":"2","number_dislikes":"3","text":"A surprising truly well-done simulation that puts you behind a truck letting you enjoy discovering and exploring Europe."},
+        {"game_name":"The Forest","username":"Nelson","number_likes":"2","number_dislikes":"3","text":"The Forest is an awkward open-world survival game. It's thrilling to play because of the intense atmosphere, but confusing maps and annoying enemies make it really hard for me to immerse into the game."},
+        {"game_name":"Hearthstone","username":"Nelson","number_likes":"2","number_dislikes":"3","text":"Between its friendly design and its flexible approach to a free-to-play economy, Hearthstone has successfully pulled me into a genre that I didn't care about in the least. But even more incredibly, it's kept me rapt longer than any games I've played in years, and shows no signs of letting up."},
+        {"game_name":"OVERWATCH","username":"EllisonZhang","number_likes":"2","number_dislikes":"3","text":"A gaming experience that’s more polished and exciting than I have ever had the pleasure of experiencing and one I’ll be sure to continue to play for the foreseeable future. If not for the inclusion of microtransactions, this would have been a perfect game in my book."},
+        {"game_name":"Stardew Valley","username":"EllisonZhang","number_likes":"2","number_dislikes":"3","text":"The charming RPG Stardew Valley is so much more than just another farming simulator and feels right at home on the Switch."},
+        {"game_name":"WORLD OF WARCRFT","username":"EllisonZhang","number_likes":"2","number_dislikes":"3","text":"Battle for Azeroth, despite the endless grind, has been an exciting expansion this far. However, raids and additional content are what makes the game great. Blizzard has made the entry level into the new expansion welcoming for new players and some die-hard critics have shared their concerns with the current direction Blizzard are going with making the gaming “easier.”"},
     ]
 
     users = [
-        {"game_name":"ARK:Survival Invovled","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"The Binding of Isaac: Rebirth","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"Don't Starve Together","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"Euro Truck Simulator 2","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"The Forest","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"Hearthstone","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"OVERWATCH","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"Stardew Valley","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
-        {"game_name":"WORLD OF WARCRFT","user_name":"czx","user_email":"347748393@gmail.com","user_password":"hdekddj29348","user_picture":"good"},
+        {"username":"EllisonZhang","password":"hdekddj29348","photo":"/static/leopard.jpg"},
+        {"username":"Nelson","password":"hdekddj29348","photo":"/static/antilope.jpg"},
+        {"username":"EdmundKuras","password":"hdekddj29348","photo":"/static/zebra.jpg"},
+        {"username":"KevinKengne","password":"hdekddj29348","photo":"/static/tigre.jpg"},
+        {"username":"GerardoAragonCamarasa","password":"hdekddj29348","photo":"/static/elephant.jpg"},
     ]
 
 
@@ -166,8 +173,14 @@ def populate():
             if video["game_name"] == game.name:
                 add_video(game,video["video_path"])
     
+    for user in users:
+        add_user(user["username"], user["password"], user["photo"])
     
-
+    for game in Game.objects.all():
+        for user in CustomUser.objects.all():
+            for review in reviews:
+                if review["username"] == user.username and game.name == review["game_name"]:
+                    add_review(game, user, review["number_likes"], review["number_dislikes"], review["text"])
 
 
 def add_publisher(publisher,country):
@@ -190,6 +203,16 @@ def add_video(game,path):
     vi = Video.objects.get_or_create(game_name=game,video_path=path)[0]
     vi.save()
     return vi
+
+def add_user(user_name, pass_word, pho_to):
+    u = CustomUser.objects.get_or_create(username=user_name, password=pass_word,photo=pho_to)[0]
+    u.save()
+    return u
+
+def add_review(game, user_used, numlikes, numdislikes, text):
+    r = Review.objects.get_or_create(number_likes=numlikes, number_dislikes=numdislikes, comment_text=text, game_reviewed=game, user=user_used)[0]
+    r.save()
+    return r
 
 # Start execution here!
 if __name__ == '__main__':
